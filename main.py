8cdaf4743a6817990c2b7e4494d23420d55db431
Zacharia Hammad <zacharia.hammad@visionarysolutions.com>
2025-12-03 16:18:24 -0500

    word count

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+def count_word(words):
+    lst = words.split()
+    return len(lst)
+
 def get_book_text(fp):
     with open(fp) as f:
         file_contents = f.read()
         return file_contents
 
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    num_words = count_word(get_book_text("books/frankenstein.txt"))
+    print(f"Found {num_words} total words")
 
 main()
 
