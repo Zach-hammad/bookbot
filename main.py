31518ce3d78c0b48d1062e89148301ae955dd662
Zacharia Hammad <zacharia.hammad@visionarysolutions.com>
2025-12-04 10:14:49 -0500

    arguments

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_word, count_char, sort_dict
+import sys
 
 def get_book_text(fp):
     with open(fp) as f:
@@ -6,16 +7,21 @@ def get_book_text(fp):
         return file_contents
 
 def main():
-    num_words = count_word(get_book_text("books/frankenstein.txt"))
-    print(f"{" BOOKBOT ":=^31}")
-    print(f"Analyzing book found at books/frankenstein.txt...")
-    print(f"{" Word Count ":-^31}")
-    print(f"Found {num_words} total words")
-    print(f"{" Character Count ":-^31}")
-    num_char = count_char(get_book_text("books/frankenstein.txt"))
-    sorted_dict = sort_dict(num_char)
-    for i in sorted_dict:
-        print(f"{i["char"]}: {i["num"]}")
-    print(f"{" END ":=^31}")
+    if len(sys.argv) == 2:
+        num_words = count_word(get_book_text(sys.argv[1]))
+        print(f"{" BOOKBOT ":=^31}")
+        print(f"Analyzing book found at {sys.argv[1]}...")
+        print(f"{" Word Count ":-^31}")
+        print(f"Found {num_words} total words") 
+        print(f"{" Character Count ":-^31}")
+        num_char = count_char(get_book_text(sys.argv[1]))
+        sorted_dict = sort_dict(num_char)
+        for i in sorted_dict: 
+            print(f"{i["char"]}: {i["num"]}")
+        print(f"{" END ":=^31}")
+        sys.exit(0)
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 main()
 
