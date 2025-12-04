6c09cdead2e2f0c98d9d6a1beb2240674896791b
Zacharia Hammad <zacharia.hammad@visionarysolutions.com>
2025-12-04 09:09:40 -0500

    count char

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_word
+from stats import count_word, count_char
 
 def get_book_text(fp):
     with open(fp) as f:
@@ -8,6 +8,7 @@ def get_book_text(fp):
 def main():
     num_words = count_word(get_book_text("books/frankenstein.txt"))
     print(f"Found {num_words} total words")
-
+    num_char = count_char(get_book_text("books/frankenstein.txt"))
+    print(num_char)
 main()
 
--- stats.py
+++ stats.py
@@ -1,3 +1,19 @@
 def count_word(words):
     lst = words.split()
     return len(lst)
+
+def count_char(words):
+    words = list(words)
+    char = {}
+    try:
+        for i in words:
+            i = i.lower()
+            if i.isalpha() and i in char:
+                char[i] += 1 
+            elif i.isalpha() and i not in char:
+                char[i] = 1
+            else:
+                continue
+    except Exception as e:
+        print(e)
+    return char
